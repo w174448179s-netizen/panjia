0cbe85f3a1606a4e40474854f7006208c9259bd6
w174448179s-netizen <w174448179s@gmail.com>
2026-08-14 23:17:24 +0800

    phase3: clean up RuoYi branding residuals
    - Delete .github/FUNDING.yml (RuoYi sponsorship)
    - Rewrite README.md from RuoYi original to PanJia
    - Update LICENSE copyright to 2026 PanJia
    - Replace RuoYi branding in register.html (title, logo alt, welcome text, footer, terms link)
    - Fix error page titles: RuoYi → 盘家 (404/500/403/service)
    - Update startup banner: 若依启动成功 → 盘家启动成功
    - Fix Druid console credentials: ruoyi/123456 → panjia/panjia_dev
    - Fix generator.yml author: ruoyi → panjia
    - Clean RuoYi references in index.js, server.html, skins.css comments
    - Update GenUtils regex to also strip 盘家 prefix from generated table names

--- ruoyi-admin/src/main/resources/static/ruoyi/index.js
+++ ruoyi-admin/src/main/resources/static/ruoyi/index.js
@@ -336,7 +336,7 @@ $(function() {
     }
 
     function menuBlank() {
-    	// 新窗口打开外网以http://开头，如http://ruoyi.vip
+    	// 新窗口打开外网以http://开头，如http://example.com
     	var dataUrl = $(this).attr('href');
     	window.open(dataUrl);
     	return false;
