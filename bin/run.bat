dc6452f0cdfbdc115e55c2d82e607881620d3c38
w174448179s-netizen <w174448179s@gmail.com>
2026-08-15 00:39:48 +0800

    checkpoint before package rename: com.ruoyi -> com.panjia

--- bin/run.bat
+++ bin/run.bat
@@ -4,7 +4,7 @@ echo [��Ϣ] ʹ��Jar��������Web���̡�
 echo.
 
 cd %~dp0
-cd ../ruoyi-admin/target
+cd ../panjia-admin/target
 
 set JAVA_OPTS=-Xms256m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m
 
